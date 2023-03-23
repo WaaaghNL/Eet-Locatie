f9ee9aaa7dddab31938af09a86c9cb22ca8cf20f
Ronald <vanheugten.ronald@gmail.com>
2023-03-23 01:29:45 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Eet-Locatie
+
+## Over de Eet Locatie Generator
+### Wat is de ELG?
+De eet locatie generator is een project van Waaagh.nl en haalt links uit het overzicht van restaurants die de Smullertjes nog willen uit proberen. Deze lijst wordt door de dino in een bak met lootjes gegooid en daarna trekt hij er een uit.
+
+### Waar kan ik de hele lijst zien?
+De hele lijst die in de ELG gaat is te vinden op de volgende webpagina: [Waaagh.nl - Restaurants TODO](https://www.waaagh.nl/Restaurants_ToDo)
---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+$json = file_get_contents('https://www.waaagh.nl/api.php?format=json&action=query&titles=Restaurants_ToDo&prop=revisions&rvprop=content');
+$json = json_decode($json, true);
+
+$json = $json["query"]["pages"][812]["revisions"][0]["*"];
+
+$json = str_ireplace('Naast een lijst van goede restaurantjes heb ik natuurlijk ook nog een ToDo lijst met aanraders van derden. [https://eetlocatie.waaagh.nl/ Laat de dino je helpen om een keuze te maken!]',null,$json);
+$json = trim($json);
+
+$array = preg_replace('/(===.+?)+(===)/i', null, $json); //Verwijder Tabel
+$array = preg_replace('/(==.+?)+(==)/i', null, $array); //Verwijder Tabel
+$array = explode("*", $array);
+foreach ($array as $key => $value){
+    $data = trim($array[$key]);
+    if(!empty($data)){
+        $output[$key] = $data;
+    }
+}
+shuffle($output); //Random output
+$count = rand(1, count($output));
+
+$url = $output[$count];
+$name = $url;
+$name = str_replace("https://", "", $name);
+$name = str_replace("http://", "", $name);
+$name = str_replace("www.", "", $name);
+if(substr($name, -1) == '/') {
+    $name = substr($name, 0, -1);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Waaagh.nl - Eet Locatie Generator</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+<body>
+
+<div id="header">
+	<div id="header-content"></div>
+</div>
+<div id="main">
+    <div id="main-content">
+        <div id="password-panel">
+            <div id="password-panel-message">Wij gaan eten bij...</div>
+        	<div id="password"><a id="password-input" href="<?=$output[$count];?>"><?=$name;?></a></div>
+        	<div id="password-panel-reset">
+        	    <a class="gradient-button gradient-button-TEST">Andere Locatie</a>
+        	</div>
+        	
+        </div>
+    
+        <div id="about-passwords" class="content">
+        	<h2>Over de Eet Locatie Generator</h2>
+        	<h3>Wat is de ELG?</h3>
+        	<p>De eet locatie generator is een project van Waaagh.nl en haalt links uit het overzicht van restaurants die de Smullertjes nog willen uit proberen. Deze lijst wordt door de dino in een bak met lootjes gegooid en daarna trekt hij er een uit.</p>
+        
+        	<h3>Waar kan ik de hele lijst zien?</h3>
+        	<p>De hele lijst die in de ELG gaat is te vinden op de volgende webpagina: <a href="https://www.waaagh.nl/Restaurants_ToDo" target="_blank">Waaagh.nl - Restaurants TODO</a></p>
+        </div>
+    </div>
+</div>
+
+<div id="footer">
+  <p>
+	<a href="https://www.waaagh.nl">  &copy; Waaagh.nl <span id="curr_year"></span> All right reserved</a> | <a href="https://github.com/WaaaghNL/Eet-Locatie">Github Page</a>.
+  </p>
+</div>
+
+<script>document.getElementById("curr_year").innerHTML = new Date().getFullYear()</script>
+
+</body>
+</html>
